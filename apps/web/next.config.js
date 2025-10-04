e6c76ee8914207527cf9e7fb0438f18592af48cb
홍규진 <hkj0206@dgu.ac.kr>
2025-10-05 01:38:12 +0900

    chore: pakcage 명 수정

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    emotion: true,
+  },
+  transpilePackages: ["@snud2025/ui"],
+  experimental: {
+    externalDir: true,
+  },
+};
 
 export default nextConfig;
