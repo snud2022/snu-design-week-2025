2a18729c8cd476af483804ba736fd4900fcf0510
홍규진 <hkj0206@dgu.ac.kr>
2025-11-13 22:49:17 +0900

    feat: implement About page with styled components, image, and content sections

---
+++ apps/web/app/about/page.style.ts
@@ -0,0 +1,86 @@
+"use client";
+import styled from "@emotion/styled";
+import { mq } from "@snud2025/ui";
+
+export const Wrapper = styled.div({
+  padding: "40px 0px",
+  maxWidth: "1200px",
+  margin: "0 auto",
+  display: "flex",
+  flexDirection: "row",
+  gap: "40px",
+  overflowY: "auto",
+  overflowX: "hidden",
+  width: "100%",
+  [mq.tablet]: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    maxWidth: "none",
+    padding: "40px 20px",
+  },
+  [mq.mobile]: {
+    flexDirection: "column",
+    alignItems: "center",
+    maxWidth: "none",
+  },
+});
+
+export const PosterContainer = styled.div({
+  position: "sticky",
+  top: "0",
+  alignSelf: "flex-start",
+  flexShrink: "0",
+  height: "fit-content",
+  "& img": {
+    width: "100%",
+    height: "auto",
+  },
+  [mq.tablet]: {
+    position: "relative",
+    alignSelf: "center",
+    display: "flex",
+    justifyContent: "center",
+  },
+  [mq.mobile]: {
+    position: "relative",
+    alignSelf: "center",
+    display: "flex",
+    justifyContent: "center",
+
+    "& img": {
+      width: "100%",
+      height: "auto",
+      objectFit: "contain",
+      margin: "0 auto",
+      padding: "0 20px",
+    },
+  },
+});
+
+export const ContentContainer = styled.div({
+  flex: "1",
+
+  [mq.tablet]: {
+    width: "100%",
+    maxWidth: "580px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  [mq.mobile]: {
+    width: "100%",
+    maxWidth: "342px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
+
+export const DescriptionSection = styled.div({
+  marginTop: "40px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "24px",
+  justifyContent: "center",
+});
---
+++ apps/web/app/about/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import * as S from "./page.style";
+import Image from "next/image";
+import EventHeader from "./components/EventHeader/EventHeader";
+import Description from "./components/Description/Description";
+import CommitteeSection from "./components/CommitteeSection/CommitteeSection";
+import HelperSection from "./components/HelperSection/HelperSection";
+import {
+  HELPER_SECTION,
+  LEADERSHIP,
+  TEAMS,
+  ABOUT_CONTENT,
+  EVENT_INFO,
+  EVENT_TITLE,
+  ROPE_FRAME_CONFIG,
+} from "./constants";
+
+export default function About() {
+  return (
+    <S.Wrapper>
+      <S.PosterContainer>
+        <Image src="/about/poster.jpg" alt="about" width={580} height={896} />
+      </S.PosterContainer>
+      <S.ContentContainer>
+        <EventHeader
+          title={EVENT_TITLE}
+          eventInfo={EVENT_INFO}
+          ropeFrameConfig={ROPE_FRAME_CONFIG}
+        />
+        <S.DescriptionSection>
+          <Description content={ABOUT_CONTENT} />
+          <CommitteeSection leadership={LEADERSHIP} teams={TEAMS} />
+          <HelperSection data={HELPER_SECTION} />
+        </S.DescriptionSection>
+      </S.ContentContainer>
+    </S.Wrapper>
+  );
+}
