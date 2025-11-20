acf1ca8de82d310a35ecf1feb6433f3525b5557e
홍규진 <hkj0206@dgu.ac.kr>
2025-11-20 17:17:46 +0900

    feat: add PartnerLogos component with styled layout and responsive design

---
+++ apps/web/app/partners/components/PartnerLogos/PartnerLogos.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import type { Partner } from "../../types/partner";
+import * as S from "./PartnerLogos.style";
+
+interface PartnerLogosProps {
+  partners: Partner[];
+}
+
+export default function PartnerLogos({ partners }: PartnerLogosProps) {
+  return (
+    <S.LogosWrapper>
+      {partners.map((partner) => (
+        <Image
+          key={partner.id}
+          src={partner.logo}
+          alt={partner.name}
+          width={partner.width}
+          height={partner.height}
+        />
+      ))}
+    </S.LogosWrapper>
+  );
+}
