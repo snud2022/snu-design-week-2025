a0042f7a4b530b668aad8305d9ac69c7d31ed6b0
홍규진 <hkj0206@dgu.ac.kr>
2025-12-01 15:15:39 +0900

    feat: Add event details for Hi-Day and Talk Concert sections

---
+++ apps/web/app/program/constants/detailSections.ts
@@ -0,0 +1,93 @@
+import type { HiDaySection, TalkConcertSection } from "../types/detailSection";
+
+export const HI_DAY_SECTION: HiDaySection = {
+  title: {
+    kr: "HI DAY / 하이데이",
+    en: "HI DAY",
+  },
+  subtitle: "서울디자이너스네트워크와 함께하는",
+  date: "2025.12.5 (금)",
+  time: "18:30 - 21:00",
+  location: {
+    kr: "서울대학교 49동 B1 미디어 월",
+    en: "Seoul National University Building 49, B1 Media Wall",
+  },
+  description: {
+    kr: "2025 SNU DESIGN WEEK를 축하하는 동시에 49동의 마지막을 기념하는 자리입니다. 재학생과 졸업생이 모여 49동에서 많은 추억들을 WRAP UP하고 한층 더 새로워질 디자인과의 공간과 비전을 함께 그려가는 뜻깊은 시간이 될 것입니다. 케이터링과 주류, 미니게임 및 이벤트가 준비되어 있으며 공식 식순 진수로 파티가 진행됩니다.",
+  },
+  schedules: [
+    {
+      time: "18:30 - 19:30",
+      event: "오프닝 공연 (DJ yumeme)",
+    },
+    {
+      time: "19:30 - 19:40",
+      event: "개회사",
+    },
+    {
+      time: "19:40 - 20:10",
+      event: "49동 리노베이션 설계안 발표 & 후원설명회 (소리요 이진태 대표)",
+    },
+    {
+      time: "20:10 - 21:00",
+      event: "네트워킹 파티 및 공연 (DJ Yves & GODOT)",
+    },
+    {
+      time: "21:00 - 21:30",
+      event: "폐회사 및 클로징 공연 (DJ GODOT)",
+    },
+  ],
+};
+
+export const TALK_CONCERT_SECTION: TalkConcertSection = {
+  title:
+    "4명의 현직 디자이너들과 진행하는\n2025 SNU DESIGN WEEK \nWRAP UP 토크콘서트: MOVING DESIGN",
+  dateRange: "2025.12.5 (금) - 2025.12.6 (토)",
+  location: {
+    kr: "서울대학교 49동 322호",
+    en: "Seoul National University Building 49, Room 322",
+  },
+  sessions: [
+    {
+      date: "12.5 (금)",
+      title: "VD TALK",
+      speakers: [
+        {
+          name: "함영훈 디자이너",
+          nameEn: "Ham Young-hoon",
+          time: "15:00 - 16:00",
+          subtitle: "디자이너의 다양한 '이사' 과정에서 가져가야 할 것들",
+          bio: "그래픽 디자이너이자 순수 예술가. 네이버를 비롯한 여러 기업에서 그래픽 디자이너로 활동했으며, 이후 프리랜서로 전환하며 뉴미디어로 작업 영역을 확장했다. 현재 브랜드 디자인 에이전시 Drkr를 운영하며 다양한 기업 및 기관의 그래픽 및 브랜딩 프로젝트를 수행하고 있다. 주요 클라이언트로는 현대카드, 카카오, 아모레퍼시픽, 신한카드, 하나금융그룹, 롯데월드타워, 국립현대미술관, 평창 2018 올림픽 등이 있으며, 작품은 국립현대미술관, 한강미술공원, 아모레퍼시픽미술관 등에 소장되어 있다.",
+        },
+        {
+          name: "정우경 디자이너",
+          nameEn: "Jung Woo-kyung",
+          time: "18:30 - 19:30",
+          subtitle: "다난번에 이사를 마치고 자가로 근데 향이 없는",
+          bio: "조안앤스테이시, 어도어에서 인하우스 디자이너로 활동했으며, 현재 Studio Hooyounghoo를 운영하고 있다. 엔터테인먼트 및 브랜딩 등 그래픽 디자인의 전 영역에서 활동하며, 레터링 및 타이포그래피 등 이미지와 텍스트를 다루는 작업에 특히 관심이 있다.",
+        },
+      ],
+    },
+    {
+      date: "12.6 (토)",
+      title: "ID TALK",
+      speakers: [
+        {
+          name: "송봉규 디자이너",
+          nameEn: "Song Bong-gyu",
+          time: "14:00 - 15:00",
+          subtitle:
+            "From Craft to Technology (공예에서 하이테크로)\n From Object to Branding (오브제에서 브랜드로)\n From Material to Program (재료에서 프로그램으로)",
+          bio: "서울에서 산업디자인을 공부했으며, 삼성전자에서 근무한 후 2010년 BKID를 설립했다. 공예에서 하이테크까지 다양한 분야에 대한 호기심을 가지고 있으며, 기업, 스타트업, 아티스트와 협업하여 새로운 기술과 스토리를 산업디자인과 융합하고 있다. BMW, Alessi, DELL, Disney 등 글로벌 기업과 협업했으며, 2015년 문화체육관광부 선정 젊은 예술가로 선정되었다. 2012년 중앙일보 선정 한국을 대표하는 디자이너 10인에 선정되었으며, 2013년 POSCO 선정 한국을 이끌 젊은 리더에 선정되었다. 2020년부터 현재까지 iF Design Award 심사위원으로 활동하고 있다.",
+        },
+        {
+          name: "성정기 디자이너",
+          nameEn: "Sung Jung-ki",
+          time: "16:00 - 17:00",
+          subtitle: "변화없는 디자인, 변화하는 프로세스",
+          bio: "LG, IDEO, LUNA를 거쳐 현재 Daylight에 재직 중이며, 글로벌 기업 경험을 바탕으로 좋은 디자인의 가치와 깊이에 대한 인사이트와 포트폴리오 제작 노하우, 실무 디자인 경험, 디자이너로서의 삶, 더 나은 세상을 만드는 디자인에 대한 이야기를 공유할 예정이다.",
+        },
+      ],
+    },
+  ],
+};
---
+++ apps/web/app/program/constants/programs.ts
@@ -0,0 +1,59 @@
+import type { ProgramEvent } from "../types/program";
+
+export const PROGRAM_INTRO = {
+  kr: "2025 SNU Design Week WRAP UP은 하이데이와 토크 콘서트가 진행되었습니다.",
+  en: "2025 SNU Design Week WRAP UP featured 'Hi-Day' and a 'Talk Concert'.",
+};
+
+export const PROGRAMS: ProgramEvent[] = [
+  {
+    imageUrl: "/program/image.jpg",
+    date: "Dec 05",
+    time: "18:00 - 21:00",
+    title: {
+      kr: "하이데이",
+      en: "Hi-Day",
+    },
+    description: {
+      kr: "2025 SNU DESIGN WEEK의 개최를 축하하는 동시에 49동의 마지막을 기념하는 자리입니다. 재학생과 졸업생이 한데 모여 49동에서의 많은 추억을 WRAP UP하고 한층 더 새로워질 디자인과의 공간과 비전을 함께 그려가는 뜻깊은 시간이 됩니다. 케이터링과 주류, 미니게임 및 이벤트가 준비되어 있으며 공식 식순 전후로 DJ 파티가 진행됩니다.",
+    },
+  },
+  {
+    date: "Dec 05",
+    time: "15:00 - 19:30",
+    title: {
+      kr: "토크 콘서트: MOVING DESIGN VD TALK",
+      en: "Talk Concert: MOVING DESIGN VD TALK",
+    },
+    speakers: [
+      {
+        name: "함영훈",
+      },
+      {
+        name: "엄후영",
+      },
+    ],
+    description: {
+      kr: "네이버를 거쳐 현재 디자인 에이전시 Orkr를 운영하는 순수 예술가 겸 디자이너 함영훈과, 조셉앤스테이시, 어도어 등에서 디자이너로 일하다 Studio Hooyounghoo를 설립한 엄후영이 참여합니다. 두 연사 모두 그래픽 디자인과 브랜딩을 기반으로 다양한 클라이언트와 작업하며 각자의 영역을 구축해왔습니다. '이사'라는 공통 주제로, 디자이너로서 겪는 다양한 커리어 전환과 독립 과정에 대한 경험을 나눌 예정입니다.",
+    },
+  },
+  {
+    date: "Dec 06",
+    time: "14:00 - 17:00",
+    title: {
+      kr: "토크 콘서트: MOVING DESIGN ID TALK",
+      en: "Talk Concert: MOVING DESIGN ID TALK",
+    },
+    speakers: [
+      {
+        name: "송봉규",
+      },
+      {
+        name: "성정기",
+      },
+    ],
+    description: {
+      kr: "삼성전자 출신으로 BKID를 설립한 송봉규 디자이너와 LG, IDEO를 거쳐 현재 Daylight에 재직 중인 성정기 디자이너가 산업 디자인에 대한 강연을 진행합니다. 송봉규 디자이너는 '공예에서 하이테크로' 등 디자인 영역 확장에 대해 이야기하고, 성정기 디자이너는 글로벌 기업 경험을 바탕으로 '변화하는 프로세스'와 포트폴리오 노하우를 공유합니다. 강연 사이에 하드웨어 제조사 스가츠네가 자사의 솔루션을 소개하는 협력사 세션이 포함되어 있습니다.",
+    },
+  },
+];
