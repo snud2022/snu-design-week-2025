f163f0c17e48f8ead7539bcf05083c377cfc8f95
jungsun <dlwjdtjs0111@gmail.com>
2025-12-08 03:30:15 +0900

    fix: 환경변수 접두사 추가

--- packages/ui/src/utils/getImageUrl.ts
+++ packages/ui/src/utils/getImageUrl.ts
@@ -1,7 +1,7 @@
 /**
  * Notion Image url을 웹 게시 링크로 변환
  */
-const NOTION_SITE_DOMAIN = process.env.NOTION_SITE_DOMAIN;
+const NOTION_SITE_DOMAIN = process.env.NEXT_PUBLIC_NOTION_SITE_DOMAIN;
 
 export function getImageUrl(imageUrl: string, pageId: string): string {
   return `https://${NOTION_SITE_DOMAIN}/image/${encodeURIComponent(imageUrl)}?table=block&id=${pageId}&cache=v2`;
