e08a2cc5d5db9322bf284b4216c535ce8ede74f0
jungsun <dlwjdtjs0111@gmail.com>
2025-12-11 15:41:19 +0900

    style: works 디자인 디테일 수정

--- apps/web/app/works/constants/position.ts
+++ apps/web/app/works/constants/position.ts
@@ -3,35 +3,35 @@ import type { Position } from "../types/position";
 // 각 파츠의 고정 위치 (index 순서)
 export const STONE_POSITIONS: Position[] = [
   {
-    tablet: { x: 16.63, y: 103.73, rotate: 22.26 },
-    desktop: { x: -10.16, y: 141.96, rotate: 22.26 },
+    tablet: { x: 16.63, y: 113.73, rotate: 22.26 },
+    desktop: { x: -10.16, y: 151.96, rotate: 22.26 },
   },
   {
-    tablet: { x: 90.18, y: 9.92, rotate: -11.24 },
+    tablet: { x: 90.18, y: 19.92, rotate: -11.24 },
     desktop: { x: 100.64, y: 48.78, rotate: -11.24 },
   },
   {
-    tablet: { x: 11.65, y: 219.07, rotate: 22.01 },
-    desktop: { x: 219.89, y: 150.51, rotate: 22.01 },
+    tablet: { x: 11.65, y: 229.07, rotate: 22.01 },
+    desktop: { x: 219.89, y: 160.51, rotate: 22.01 },
   },
   {
-    tablet: { x: 181.05, y: 120, rotate: -7.88 },
-    desktop: { x: 455.29, y: 17, rotate: -7.88 },
+    tablet: { x: 181.05, y: 130, rotate: -7.88 },
+    desktop: { x: 455.29, y: 27, rotate: -7.88 },
   },
   {
-    tablet: { x: 320.65, y: 156.08, rotate: 10.58 },
-    desktop: { x: 587.89, y: 157.89, rotate: 10.58 },
+    tablet: { x: 320.65, y: 166.08, rotate: 10.58 },
+    desktop: { x: 587.89, y: 167.89, rotate: 10.58 },
   },
   {
-    tablet: { x: 240.55, y: 259.88, rotate: -8.22 },
-    desktop: { x: 718.63, y: 74.02, rotate: -8.22 },
+    tablet: { x: 240.55, y: 269.88, rotate: -8.22 },
+    desktop: { x: 718.63, y: 84.02, rotate: -8.22 },
   },
   {
-    tablet: { x: 411.65, y: 257.86, rotate: 15.06 },
-    desktop: { x: 802.89, y: 190.96, rotate: 15.06 },
+    tablet: { x: 411.65, y: 267.86, rotate: 15.06 },
+    desktop: { x: 802.89, y: 200.96, rotate: 15.06 },
   },
   {
-    tablet: { x: 303.63, y: 15.88, rotate: -24.48 },
-    desktop: { x: 920.83, y: 58.02, rotate: -24.48 },
+    tablet: { x: 303.63, y: 45.88, rotate: -24.48 },
+    desktop: { x: 920.83, y: 68.02, rotate: -24.48 },
   },
 ];
